5477a57c8f05f412626c0e7614f4d480adb4d753
fhenri42 <fhenri@student.42.fr>
2016-10-03 18:43:22 +0200

    [UPD] readme.md

---
+++ config.js
@@ -0,0 +1,10 @@
+const config = {}
+
+config.appSecret = 'APP_SECERT'
+config.pageAccessToken = 'PAGE_TOKEN'
+config.validationToken = 'ABCD1234'
+config.serverURL = 'https://*******.ngrok.io'
+config.recastToken = 'RECAST_TOKEN'
+config.language = 'en'
+
+module.exports = config
