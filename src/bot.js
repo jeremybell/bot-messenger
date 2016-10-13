b7057ad3f7b6f78e7b0e80b0e60c4fc038a5e354
fhenri42 <fhenri@student.42.fr>
2016-10-13 14:18:27 +0200

    [UPD]change to => notion

--- src/bot.js
+++ src/bot.js
@@ -26,7 +26,7 @@ function handleMessage(event) {
       } else {
         if (action && action.done === true) {
           console.log('action is done')
-          // Use external services: use res.memory('knowledge') if you got a knowledge from this action
+          // Use external services: use res.memory('notion') if you got a notion from this action
         }
         let promise = Promise.resolve()
         replies.forEach(rep => {
